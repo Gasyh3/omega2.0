4963a231d00993152165467b52a4a3979d5b1f58
Gasyh3 <kevrakt@gmail.com>
2023-01-27 23:45:35 +0100

    Upload SVG logo and Update Contact with logo

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Contact.scss'
+import FBLogo from "../Logo/Facebook"
+import TikTokLogo from "../Logo/TikTok"
+import InstaLogo from "../Logo/Instagram"
+import WhatLogo from "../Logo/Whatsapp"
+import Youtube from '../Logo/Youtube'
 
 function Contact() {
   return (
-    <div className="contact">
-        Contact
+    <div className="contact" id="contact">
+        <section className="contact__reseaux">  
+        <FBLogo />    
+        <TikTokLogo />
+        <InstaLogo />
+        <WhatLogo />
+        <Youtube />
+        </section>
+        <section className="contact__form">
+          Form
+        </section>
     </div>
   )
 }
