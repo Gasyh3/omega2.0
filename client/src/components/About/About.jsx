e96a806c082ac55fabef4edf49b7a16dba0e17c5
Gasyh3 <kevrakt@gmail.com>
2023-01-27 22:29:09 +0100

    Update view bug on header and Update About with responsive

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -40,7 +40,10 @@ function About() {
         <article className="about__article">
           <h2 className="about__title goudy">Qui suis-je ?</h2>
           <p className="about__text goudy">
-             Coiffeuse et esthéticienne diplômée ainsi que maquilleuse certifiée et major de promo trois années consécutives, j'exerce avec passion et dévouement mon métier artistique. Faisant preuve de patience, de douceur, de bienveillance et d'attention, j'aime, le temps des préparatifs, devenir votre confidente afin de vous proposer une prestation idoine et combler le moindre de vos désirs.
+             Coiffeuse et esthéticienne diplômée ainsi que maquilleuse certifiée et major de promo trois années consécutives, j'exerce avec passion et dévouement mon métier artistique.
+             </p>
+             <p className="about__text goudy">
+             Faisant preuve de patience, de douceur, de bienveillance et d'attention, j'aime, le temps des préparatifs, devenir votre confidente afin de vous proposer une prestation idoine et combler le moindre de vos désirs.
           </p>
           <section className="about__second">
         <button className='about__btn goudy' onClick={openModal}>En savoir plus ...</button>
@@ -59,7 +62,8 @@ function About() {
           <article className="about__article left-none">
             <h2 className="about__title goudy">Pourquoi OMEGA 2.0 ?</h2>
             <p className="about__text goudy">
-              Déjà, le 2.0 reflète une manière plus évoluée, plus aboutie de fonctionner. De par les recherches d'inspirations de la part des clients ainsi qu'une prise de contact moderne, je travaille beaucoup plus en coulisse pour proposer quelque chose de précis et de personnalisé. Plus contraint de se déplacer en studio, c'est le studio qui se rapprochera de vous.
+              Déjà, le 2.0 reflète une manière plus évoluée, plus aboutie de fonctionner.
+              De par les recherches d'inspirations de la part des clients ainsi qu'une prise de contact moderne, je travaille beaucoup plus en coulisse pour proposer quelque chose de précis et de personnalisé. Plus contraint de se déplacer en studio, c'est le studio qui se rapprochera de vous.
             </p>
             <p className="about__text goudy">
               De l'Alpha à l'Omega, c'est un peu ma manière de dire que je vous accompagnerai de A à Z dans tout le processus d'un « Ô grand » projet. L'Omega, dernière lettre de l'alphabet grec, fait aussi référence à la dernière étape avant le mariage, un joli rouge à lèvre et hop, on est parti pour le grand Jour ! Omega est aussi un dérivé de « Eyn » qui signifie « Œil » en phénicien et qui me fait tout simplement penser à tous les yeux pétillants que je maquille. C'est également l'interlude d'un des albums de mon groupe préféré. Mais c'est également là 24ème étoile la plus brillante d'une constellation, et si je peux vous mettre des milliers d'étincelles dans les yeux et des papillons dans le ventre, alors j'aurais réussi mon pari 2.0 !
