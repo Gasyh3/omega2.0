e8e46c1c19590fea893b1752da526d986e2acb51
Gasyh3 <kevrakt@gmail.com>
2023-01-28 01:19:58 +0100

    Fix about size-height and update contact logo links

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -9,12 +9,23 @@ import Youtube from '../Logo/Youtube'
 function Contact() {
   return (
     <div className="contact" id="contact">
-        <section className="contact__reseaux">  
-        <FBLogo />    
-        <TikTokLogo />
-        <InstaLogo />
-        <WhatLogo />
-        <Youtube />
+        <section className="contact__reseaux">
+          <a href="https://www.whatsapp.com/?lang=fr" className="contact__link">
+            <WhatLogo />
+          </a> 
+          <a href="https://www.instagram.com/?hl=fr" className="contact__link">
+            <InstaLogo />
+          </a> 
+          <a href="https://www.youtube.com/" className="contact__link">
+            <Youtube />
+          </a> 
+          <a href="https://fr-fr.facebook.com/" className="contact__link">
+            <FBLogo /> 
+          </a>  
+          <a href="https://www.tiktok.com/fr/" className="contact__link">
+            <TikTokLogo />
+          </a> 
+        
         </section>
         <section className="contact__form">
           Form
