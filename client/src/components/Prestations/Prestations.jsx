d5139cdd0474cafad5ba1b67b6aba604730c0c5c
Gasyh3 <kevrakt@gmail.com>
2023-02-09 15:47:50 +0100

    Update Prestation with grid and responsive, fix bug on contact

--- client/src/components/Prestations/Prestations.jsx
+++ client/src/components/Prestations/Prestations.jsx
@@ -4,7 +4,14 @@ import './Prestations.scss'
 function Prestations() {
   return (
     <div className="prestations">
-        Prestations
+      <div className="prestations__container">
+        <a href="#" className="prestations__cases">Mariage</a>
+        <a href="#" className="prestations__cases">Maquillage</a>
+        <a href="#" className="prestations__cases">Coiffure</a>
+        <a href="#" className="prestations__cases">Onglerie</a>
+        <a href="#" className="prestations__cases">Press-on nails</a>
+        <a href="#" className="prestations__cases">Tatouages éphémère</a>
+      </div>
     </div>
   )
 }
