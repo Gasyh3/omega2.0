ae9c1edb475d51d630d3b1792ba93dcfb46a9bb6
Gasyh3 <kevrakt@gmail.com>
2023-02-09 11:43:40 +0100

    Update logo target= blank

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -11,19 +11,19 @@ function Contact() {
   return (
     <div className="contact" id="contact">
         <section className="contact__reseaux">
-          <a href="https://www.whatsapp.com/?lang=fr" className="contact__link">
+          <a href="https://www.whatsapp.com/?lang=fr" className="contact__link" target="_blank">
             <WhatLogo />
           </a> 
-          <a href="https://www.instagram.com/?hl=fr" className="contact__link">
+          <a href="https://www.instagram.com/?hl=fr" className="contact__link" target="_blank">
             <InstaLogo />
           </a> 
-          <a href="https://www.youtube.com/" className="contact__link">
+          <a href="https://www.youtube.com/" className="contact__link" target="_blank">
             <Youtube />
           </a> 
-          <a href="https://fr-fr.facebook.com/" className="contact__link">
+          <a href="https://fr-fr.facebook.com/" className="contact__link" target="_blank">
             <FBLogo /> 
           </a>  
-          <a href="https://www.tiktok.com/fr/" className="contact__link">
+          <a href="https://www.tiktok.com/fr/" className="contact__link" target="_blank">
             <TikTokLogo />
           </a> 
         
