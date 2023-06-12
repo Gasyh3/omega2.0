022e2b5bd26396e7d0d94f1fd7727d44640584cd
Gasyh3 <kevrakt@gmail.com>
2023-06-12 10:49:05 +0200

    Update footer

--- client/src/layout/Footer/Footer.jsx
+++ client/src/layout/Footer/Footer.jsx
@@ -4,7 +4,13 @@ import './Footer.scss'
 function Footer() {
   return (
     <div className="footer">
-        Footer
+        <p className="footer__text goudy">Maquilleuse et coiffeuse professionnelle</p>
+        <p className="footer__text  goudy">Styliste ongulaire et évènementiel</p>
+        <div className="footer__right">
+          <p className="footer__text goudy">Copyright Omega 2.0 - 2023</p>
+          <p className="footer__text goudy">Tous droits réservés</p>
+          <a href="/" className="footer__text goudy">Mentions légales</a>
+        </div>
     </div>
   )
 }
